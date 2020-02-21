aba4bedfe690fdb90f2dc41f91c5a9c91c5b543c
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-02-21 13:39:06 -0800

    default measure added, still need required alert

--- src/components/controls/DataEntryForm.js
+++ src/components/controls/DataEntryForm.js
@@ -30,7 +30,10 @@ const DataEntryForm = () => {
       diameter: Number(diameter),
       circumferenceUnit: circumferenceUnit,
       diameterUnit: diameterUnit
-    });
+    })
+      .then(() => {
+        console.log('created data point in firestore');
+      });
   };
 
   return (
@@ -41,6 +44,7 @@ const DataEntryForm = () => {
           <h3>Circumference:</h3>
           <input type='number' required value={circumference} onChange={({ target }) => setCircumference(target.value)}/>
           <select id="units" required value={circumferenceUnit} onChange={({ target }) => setCircumferenceUnit(target.value)}>
+            <option>--</option>
             <option value="cm">cm</option>
             <option value="in">in</option>
             <option value="m">m</option>
@@ -51,6 +55,7 @@ const DataEntryForm = () => {
           <h3>Diameter:</h3>
           <input type='number' required value={diameter} onChange={({ target }) => setDiameter(target.value)}/>
           <select id="units" required value={diameterUnit} onChange={({ target }) => setDiameterUnit(target.value)}>
+            <option>--</option>
             <option value="cm">cm</option>
             <option value="in">in</option>
             <option value="m">m</option>
