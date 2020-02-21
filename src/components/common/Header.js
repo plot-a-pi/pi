c6f26f55f4f731ce0767022296d59be4c053db85
Jacob Harrington <51795865+yaycub@users.noreply.github.com>
2020-02-21 15:41:10 -0800

    Component/header (#2)
    * header initial commit
    
    * header component finished and snapshot created

---
+++ src/components/common/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Header.css';
+
+const Header = () => {
+
+  return (
+    <header className={styles.Header}>
+      <h1>Plot-A-Pi</h1>
+      <h2>It&apos;s easy as Pi!</h2>
+    </header>
+  );
+};
+
+export default Header;
