04e9537abdd85f96b4160b81804b8b014cccb504
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-21 11:14:47 -0800

    stats mocked

--- src/components/home/Stats.js
+++ src/components/home/Stats.js
@@ -1,3 +1,24 @@
+import React, { useState, useEffect } from 'react';
 const Stats = () => {
 
+  const [sampleSize, setSampleSize] = useState(0);
+  const [piApproximaion, setPiApproximaion] = useState(0);
+
+  useEffect(() => {
+    //getSampleSize,
+    //setSampleSize
+    //getPiApproximaion,
+    //setPiApproximation
+  }, []);
+
+  return (
+    <>
+      <h2>Sample Size:</h2>
+      <p>{sampleSize}</p>
+      <h2>Pi Approximation:</h2>
+      <p>{piApproximaion}</p>
+    </>
+  );
 };
+
+export default Stats;
