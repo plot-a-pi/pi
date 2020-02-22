a2a70c5a2cfcf9ba1df46cc1c26594015973d903
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-21 16:33:02 -0800

    functions

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,17 @@
-// const functions = require('firebase-functions');
-// const firebase = require('firebase');
-// const admin = require('firebase-admin');
-// admin.initializeApp();
+const functions = require('firebase-functions');
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
+
+const dataPointCollection = db.collection('data-points');
+
+exports.getDocs = functions.https.onRequest((req, res) => {
+    dataPointCollection.get().then(docs => {
+        res.send(docs)
+    })
+}
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
