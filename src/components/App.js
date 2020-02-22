f3ff042c55e350dc17eb93c559fb2d7683f4c22c
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-02-22 09:34:43 -0800

    revert app back to standard

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ScatterGraph from '../components/graphs/ScatterGraph';
-import dartsArray from '../data/montecarlo';
 
 export default function App() {
   return (
-    <ScatterGraph data={dartsArray} xMax={1} yMax={1} xLabel={'x'} yLabel={'y'} title={'Monte Carlo Pi Approximation'} />
+    <h1>Hello World</h1>
   );
 }
--- src/components/graphs/MonteCarlo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ScatterGraph from '../graphs/ScatterGraph';
-import { generateMonteCarloData } from '../../data/montecarlo';
-
-const MonteCarlo = () => {
-  
-}
-;
