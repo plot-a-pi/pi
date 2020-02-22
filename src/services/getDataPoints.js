9fac59299ca5f76f84f75d0db4118ccf1f0e9838
Jacob Harrington <jharrington4@gmail.com>
2020-02-21 17:38:58 -0800

    mutable getData service complete

---
+++ src/services/getDataPoints.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCqogSdWZ-jW3hcCe-eljYKUL2Rsr6zVZo',
+  authDomain: 'plot-a-pi.firebaseapp.com',
+  databaseURL: 'https://plot-a-pi.firebaseio.com',
+  projectId: 'plot-a-pi',
+  storageBucket: 'plot-a-pi.appspot.com',
+  messagingSenderId: '470192475850',
+  appId: '1:470192475850:web:27390054dc9be10153ee33'
+});
+
+export const getDataPoints = (dbname, dispatch) => {
+  const db = firebase.firestore().collection(dbname);
+
+  return db.
+    onSnapshot(data => {
+      dispatch(data.docs.map(data => ({ ...data.data() })));
+    });
+};
