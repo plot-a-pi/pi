54c2846d8f7c3cc68186ec7dfe28250286a44a86
Tali Avni <tali@aristo7.com>
2020-02-24 14:35:29 -0800

    circumference vs diameter graph finished

--- src/components/graphs/CircumferenceVsDiameterGraph.js
+++ src/components/graphs/CircumferenceVsDiameterGraph.js
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import Scatterplot from './Scatterplot'
-import { getCircumferenceVsDiameterArray } from '../../services/circumferenceDiameter'
 import { useFirestore } from '../../firebase/hooks'
 import { globalDataCollection, globalStatsCollection } from '../../firebase/firebase'
 
 const CircumferenceVsDiameterGraph = () => {
-
+    let yMax = 100
+    let xMax = 100
+    let dataArray = []
     const data = useFirestore(globalDataCollection)
+    const stats = useFirestore(globalStatsCollection)
     console.log(data, 'data')
     if(!data) return <h1>no data</h1>
-    const dataArray = (data.map(point => [point.circumference, point.diameter]))
+    dataArray = (data.map(point => [point.circumference, point.diameter]))
+
+    if(stats && stats.circumferenceMax && stats.diameterMax) {
+        xMax = stats.circumferenceMax
+        yMax = stats.diameterMax
+    }
 
     return (
     <>
     <h1>Graph</h1>
-        <Scatterplot data={dataArray} xMax={100} yMax ={100}/>
+        <Scatterplot data={dataArray} xMax={xMax} yMax ={yMax}/>
     </>
     )
 }
