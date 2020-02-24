953f988eacc8d8c6210db240497016eac1af8d31
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-24 11:59:09 -0800

    children

--- src/components/common/CSV.js
+++ src/components/common/CSV.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { PropTypes } from 'prop-types';
 import { CSVLink } from 'react-csv';
 
-const CSV = ({ csvData, header1, header2  }) => {
+const CSV = ({ csvData, header1, header2, children }) => {
 
   //csvData takes form array of couplets
   //header1 & header2 sit at the top of the file eg circumfrence diameter
@@ -15,14 +15,18 @@ const CSV = ({ csvData, header1, header2  }) => {
     { label: header2, key: 'y' }
   ];
   return (
-    <CSVLink data={csvDataPreparedForHeaders} headers={headers} target='_blank' />
+    <CSVLink data={csvDataPreparedForHeaders} headers={headers} target='_blank'>
+      {children}
+    </CSVLink>
+    
   );
 };
 
 CSV.propTypes = {
   csvData : PropTypes.array.isRequired,
   header1 : PropTypes.string,
-  header2 : PropTypes.string
+  header2 : PropTypes.string,
+  children : PropTypes.nodes
 };
 
 export default CSV;
