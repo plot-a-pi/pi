65e8c86ae9447c65dad38f56caa91e062adb321e
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-24 11:40:50 -0800

    Prop types

--- src/components/common/CSV.js
+++ src/components/common/CSV.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { CSVLink, csvData } from 'react-csv';
+import { CSVLink } from 'react-csv';
 
 const CSV = ({ csvData, header1, header2  }) => {
 
   //csvData takes form array of couplets
-  const csvDataPreparedForHeaders = csvData.map(datum => ({ x : datum[0] }, { y : datum[1] }));
+  //header1 & header2 sit at the top of the file
+
+  const csvDataPreparedForHeaders = csvData.map(datum => ({ 'x' : datum[0] }, { 'y' : datum[1] }));
 
 
   const headers = [
@@ -15,6 +17,12 @@ const CSV = ({ csvData, header1, header2  }) => {
   return (
     <CSVLink data={csvDataPreparedForHeaders} headers={headers} target='_blank' />
   );
+};
+
+CSV.propTypes = {
+  csvData : PropTypes.arrray.isRequired,
+  header1 : PropTypes.string,
+  header2 : PropTypes.string
 };
 
 export default CSV;
