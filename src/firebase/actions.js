e0fdf9ecc60b6f99e3fd1e60ce2551c93b76e2b5
Jacob Harrington <jharrington4@gmail.com>
2020-02-24 15:37:49 -0800

    createSessionData working and also posting to globalDataCollection

--- src/firebase/actions.js
+++ src/firebase/actions.js
@@ -3,3 +3,7 @@ import { globalDataCollection } from './firebase';
 
 export const createSession = (teacherId, sessionName) => sessionDataCollection.add({ teacherId, name: sessionName });
 export const createDataPoint = dataPoint => globalDataCollection.add(dataPoint);
+export const createSesssionData = (sessionId, data) => {
+  sessionDataCollection.doc(sessionId).collection(sessionId).add(data);
+  globalDataCollection.add(data);
+};
