6c7a1ab323e2b75c71c0de058d11c0700b13ed40
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-02-24 11:54:17 -0800

    update testing snapshot

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import DataEntryForm from './controls/DataEntryForm';
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <DataEntryForm />
   );
 }
+
+{/* <h1>Hello World</h1> */}
