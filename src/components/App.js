956c894b242cd4e100d9af014b2fdadc60818cf3
Jacob Harrington <jharrington4@gmail.com>
2020-02-24 14:46:51 -0800

    useCSV hook working with ref

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Test from './home/Home';
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>test test test</h1>
+      <Test />
+    </>
   );
 }
 
---
+++ src/hooks/useCSV.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { useFirestore } from '../firebase/hooks';
+
+const useCSV = (ref) => {
+  const [csvStatus, setCsvStatus] = useState({ 
+    ready: false,
+    data: [] 
+  });
+  const fireStoreData = useFirestore(ref, []);
+
+  const handleClick = () => {
+    return setCsvStatus({
+      ready: true,
+      data: fireStoreData.map(entry => (
+        [entry.diameter, entry.circumference]
+      ))
+    });
+  };
+
+  
+
+  return { csvStatus, handleClick }; 
+};
+
+export default useCSV;
