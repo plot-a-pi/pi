cc6751432c587cb814628246a67aeab74479e545
Lisa Carpenter <48000707+licarpen@users.noreply.github.com>
2020-02-24 11:59:20 -0800

    MonteCarlo simulation and improved ScatterPlot components (#12)
    * add file structure for components
    
    * add generateMonteCarloData function
    
    * revert app back to standard
    
    * change file scaffolding to accomodate montecarlo page and add MonteCarlo container component
    
    * add tested montecarlo actions
    
    * add passing snapshot test for MonteCarloDartsGraph component
    
    * add tested monteCarlo reducer
    
    * add selectors for MonteCarlo and MonteCarloControls component
    
    * add functionality of adding 1 dart to graph and updating pi
    
    * add pi approximation vs sample number
    
    * add zooming, scrolling, and window-responsiveness to Scatterplot component
    
    * add passing tests for monteCarlo reducers
    
    * fix caseing error for testing
    
    * add file import for travis
    
    * remove js.fn from mock file
    
    * add file extensions to testing imports
    
    * Rename montecarloActions.js to monteCarloActions.js
    
    Change filename casing
    
    * Rename montecarlo.js to monteCarlo.js
    
    Change file name casing
    
    * change filename for travis
    
    * revert filename
    
    * Update monteCarloReducer.js
    
    change file name to correct casing

---
+++ src/containers/MonteCarlo.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react';
+import MonteCarloControls from '../components/monteCarlo/MonteCarloControls';
+// import MonteCarloDartsGraph from '../components/monteCarlo/MonteCarloDartsGraph';
+import Scatterplot from '../components/graphs/Scatterplot';
+import monteCarloReducer from '../reducers/monteCarloReducer';
+import { getPiApproximation, getDartsTotal, getDartsArray, getNumDartsVersusPiArray } from '../selectors/monteCarloSelectors';
+import { add1Dart, add10Darts, add100Darts, add1000Darts, clearDarts } from '../actions/monteCarloActions';
+
+const MonteCarlo = () => {
+  const [piState, dispatch] = useReducer(monteCarloReducer, { piApproximation: null, dartsTotal: 1, circleTotal: 1, dartsArray: [], piApproximationsArray: [] });
+
+  const actions = [
+    { name: 'ADD_1_DART', text: '1', actionCreator: () => dispatch(add1Dart()) },
+    { name: 'ADD_10_DARTS', text: '10', actionCreator: () => dispatch(add10Darts()) },
+    { name: 'ADD_100_DARTS', text: '100', actionCreator: () => dispatch(add100Darts()) },
+    { name: 'ADD_1000_DARTS', text: '1000', actionCreator: () => dispatch(add1000Darts()) },
+    { name: 'CLEAR_DARTS', text: 'Reset', actionCreator: () => dispatch(clearDarts()) }
+  ];
+
+  // change to select
+
+  const piApproximation = getPiApproximation(piState);
+  const dartsTotal = getDartsTotal(piState);
+  const dartsArray = getDartsArray(piState);
+  const numDartsVersusPiArray = getNumDartsVersusPiArray(piState);
+
+  return (
+    <>      
+      <h1>{piApproximation}</h1>
+      <Scatterplot data={dartsArray} xMax={1} yMax={1} />
+      <MonteCarloControls actions={actions} />
+      <Scatterplot data={numDartsVersusPiArray} xMax={dartsTotal} yMax={5} />
+    </>
+  );
+};
+
+export default MonteCarlo;
---
+++ src/data/__mocks__/monteCarlo.js
@@ -0,0 +1,1 @@
+export const generateMonteCarloData = () => ({ newCircleTotal: 2, newDartsArray: [[0, 0]], newPiApproximationsArray: [2] });
