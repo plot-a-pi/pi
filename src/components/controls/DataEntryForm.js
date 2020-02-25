fd8c6b9188142db835c6f57b9e3db422e78cbcc8
Jasmin Moneymaker <54380347+JMoneymaker@users.noreply.github.com>
2020-02-24 19:18:47 -0800

    Components/modal (#23)
    * basic modal in place with template styling
    
    * modal component working
    
    * remove react-modal from dependencies

--- src/components/controls/DataEntryForm.js
+++ src/components/controls/DataEntryForm.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './DataEntryForm.css';
 import { createDataPoint } from '../../firebase/actions';
 import { useFormInput } from '../../hooks/useFormInput';
+import Modal from '../common/Modal';
+import { useModal } from '../../hooks/useModal';
 import { useHistory } from 'react-router-dom';
  
 const DataEntryForm = () => {
@@ -37,13 +39,17 @@ const DataEntryForm = () => {
     history.replace('/');
   };
  
- 
+  const [showCircumferenceModal, toggleCircumferenceModal] = useModal();
+  const [showDiameterModal, toggleDiameterModal] = useModal();
+
+
   return (
     <div className={styles.DataEntryForm}>
       <h1>Plot-a-π</h1>
       <form onSubmit={handleSubmit} >
         <div>
           <h3>Circumference:</h3>
+          <Modal showModal={showCircumferenceModal} toggleModal={toggleCircumferenceModal} modalTitle={'Circumference'} modalInstructions={'This is how you do circumferency things'}/>
           <input type='text' required value={circumference} {...bindCircumference} />
           <select id="circumferenceUnits" required value={circumferenceUnit} {...bindCircumferenceUnit} >
             <option value=''></option>
@@ -52,9 +58,11 @@ const DataEntryForm = () => {
             <option value="m">m</option>
             <option value="ft">ft</option>
           </select>
+          <button className={styles.modalButton} type='button' onClick={() => toggleCircumferenceModal()}> ? </button>
         </div>
         <div>
           <h3>Diameter:</h3>
+          <Modal showModal={showDiameterModal} toggleModal={toggleDiameterModal} modalTitle={'Diameter'} modalInstructions={'This is how you do diametery things'}/>
           <input type='text' required value={diameter} {...bindDiameter} />
           <select id="diameterUnits" required value={diameterUnit} {...bindDiameterUnit}>
             <option value=''></option>
@@ -63,6 +71,7 @@ const DataEntryForm = () => {
             <option value="m">m</option>
             <option value="ft">ft</option>
           </select>
+          <button className={styles.modalButton} type='button' onClick={() => toggleDiameterModal()}> ? </button>
         </div>
         <button>Plot!</button>
       </form>
