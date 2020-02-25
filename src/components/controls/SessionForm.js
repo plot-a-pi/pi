17f8788e90daa3251184ae79b46b10535343f2f7
Jasmin Moneymaker <54380347+JMoneymaker@users.noreply.github.com>
2020-02-24 19:25:22 -0800

    Components/teacher dashboard (#29)
    * pulling from dev
    
    * Update snapshots
    
    * moving on to csv

---
+++ src/components/controls/SessionForm.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import styles from './DataEntryForm.css';
+import PropTypes from 'prop-types';
+import { createSessionData } from '../../firebase/actions';
+import { useFormInput } from '../../hooks/useFormInput';
+ 
+const SessionForm = ({ match }) => {
+  const { value: circumference, bind: bindCircumference, reset: resetCircumference } = useFormInput('');
+  const { value: circumferenceUnit, bind: bindCircumferenceUnit, reset: resetCircumferenceUnit } = useFormInput('');
+  const { value: diameter, bind: bindDiameter, reset: resetDiameter } = useFormInput('');
+  const { value: diameterUnit, bind: bindDiameterUnit, reset: resetDiameterUnit } = useFormInput('');
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const circumferenceAsNumber = Number(circumference);
+    const diameterAsNumber = Number(diameter);
+ 
+    if(Number.isNaN(circumferenceAsNumber) || Number.isNaN(diameterAsNumber)) return alert('Please enter a number.');
+    if(circumferenceAsNumber <= 0 || diameterAsNumber <= 0) return alert('Please enter a positive number.');
+    if(circumferenceUnit !== diameterUnit) return alert('Are you sure your units are correct?');
+    if(circumferenceAsNumber < diameterAsNumber) return alert('Are you sure your measurements are correct?');
+   
+    createSessionData(match.params.id, {
+      circumference: Number(circumference),
+      diameter: Number(diameter),
+      circumferenceUnit,
+      diameterUnit
+    });
+ 
+    resetCircumference();
+    resetCircumferenceUnit();
+    resetDiameter();
+    resetDiameterUnit();
+    
+    alert('Success! Your pi has been saved!');
+  };
+ 
+ 
+  return (
+    <div className={styles.DataEntryForm}>
+      <h1>Plot-a-π</h1>
+      <form onSubmit={handleSubmit} >
+        <div>
+          <h3>Circumference:</h3>
+          <input type='text' required value={circumference} {...bindCircumference} />
+          <select id="circumferenceUnits" required value={circumferenceUnit} {...bindCircumferenceUnit} >
+            <option value=''></option>
+            <option value="cm">cm</option>
+            <option value="in">in</option>
+            <option value="m">m</option>
+            <option value="ft">ft</option>
+          </select>
+        </div>
+        <div>
+          <h3>Diameter:</h3>
+          <input type='text' required value={diameter} {...bindDiameter} />
+          <select id="diameterUnits" required value={diameterUnit} {...bindDiameterUnit}>
+            <option value=''></option>
+            <option value="cm">cm</option>
+            <option value="in">in</option>
+            <option value="m">m</option>
+            <option value="ft">ft</option>
+          </select>
+        </div>
+        <button>Plot!</button>
+      </form>
+    </div>
+  );
+};
+
+SessionForm.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired
+    })
+  })
+};
+ 
+ 
+export default SessionForm;
+ 
+ 
