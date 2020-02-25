df4e0721b0a40cc6a37abeda7f06f193c1c13a31
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-24 19:06:54 -0800

    purple

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -10,47 +10,51 @@ const Nav = () => {
   if(!publicFacingNav){
     jsx = (
       <div className={styles.Big}>
-        <span onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
-        <span onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
-        <span onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
+        <span style={{ color :'rgba(221, 157, 231, 1)' }} onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
+        <span style={{ color :'rgba(221, 157, 231, 1)' }} onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
+        <span style={{ color :'rgba(221, 157, 231, 1)' }} onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
       </div>);
   }
   if(publicFacingNav){
     jsx = (
       <div className={styles.NavBackground}>
         <div id='jsxHorizontal' className={styles.Collumn}>
-          <span style={{ color: 'white', textDecoration: 'none' }} onClick={() => setPublicFacingNav(!publicFacingNav)}>|||</span>
+          <span style={{ color: '#570963', textDecoration: 'none' }} onClick={() => setPublicFacingNav(!publicFacingNav)}>|||</span>
         </div>
         <ul className={styles.Nav}>
-          <NavLink style={{ color: 'white', textDecoration: 'none' }} exact to='/' activeStyle={{
+          <NavLink style={{ color: 'blue', textDecoration: 'none' }} exact to='/' activeStyle={{
             fontWeight: 'bold',
             color: 'purple'
           }}>
             <input style={{ display: 'none' }} id='home' value='home' onChange={handleChange} />
             <label htmlFor='home'>Home</label>
           </NavLink>
-          <NavLink style={{ color: 'white', textDecoration: 'none' }}  to='/teachers' activeStyle={{
+          <br/>
+          <NavLink style={{ color: 'blue', textDecoration: 'none' }}  to='/teachers' activeStyle={{
             fontWeight: 'bold',
             color: 'purple'
           }}>
             <input style={{ display: 'none' }} id='teachers' value='teachers'  onChange={handleChange} />
             <label htmlFor='teachers'>For Teachers</label>
           </NavLink>
-          <NavLink style={{ color: 'white', textDecoration: 'none' }}  to='/pifaqs' activeStyle={{
+          <br/>
+          <NavLink style={{ color: 'blue', textDecoration: 'none' }}  to='/pifaqs' activeStyle={{
             fontWeight: 'bold',
             color: 'purple'
           }}>
             <input style={{ display: 'none' }} id='pifaqs' value='pifaqs'  onChange={handleChange} />
             <label htmlFor='pifaqs'>Pi FAQs</label>
           </NavLink>
-          <NavLink style={{ color: 'white', textDecoration: 'none' }}  to='/map' activeStyle={{
+          <br/>
+          <NavLink style={{ color: 'blue', textDecoration: 'none' }}  to='/map' activeStyle={{
             fontWeight: 'bold',
             color: 'purple'
           }}>
             <input style={{ display: 'none' }} id='map' value='map'  onChange={handleChange} />
             <label htmlFor='map'>Data Map</label>
           </NavLink>
-          <NavLink style={{ color: 'white', textDecoration: 'none' }}  to='/montecarlo' activeStyle={{
+          <br/>
+          <NavLink style={{ color: 'blue', textDecoration: 'none' }}  to='/montecarlo' activeStyle={{
             fontWeight: 'bold',
             color: 'purple'
           }}>
