a5a024ff0ba4447bdd19a5d3291f09ae442b4a11
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-24 18:21:28 -0800

    hamburger

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Nav from './common/Nav';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <Router>
+      <Nav />
+    </Router>
   );
 }
 
--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,49 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useNav } from '../../hooks/nav';
+import styles from './Nav.css';
 
 const Nav = () => {
   const { handleChange } = useNav();
-
-  return (
-    <>
-      <NavLink exact to='/' activeStyle={{
-        fontWeight: 'bold',
-        color: 'red'
-      }}>
-        <input id='home' type="radio" name='nav' value='home' onChange={handleChange} />
-        <label htmlFor='home'>Home</label>
-      </NavLink>
-      <NavLink to='/teachers' activeStyle={{
-        fontWeight: 'bold',
-        color: 'red'
-      }}>
-        <input id='teachers' type="radio" name='nav' value='teachers'  onChange={handleChange} />
-        <label htmlFor='teachers'>For Teachers</label>
-      </NavLink>
-      <NavLink to='/pifaqs' activeStyle={{
-        fontWeight: 'bold',
-        color: 'red'
-      }}>
-        <input id='pifaqs' type="radio" name='nav' value='pifaqs'  onChange={handleChange} />
-        <label htmlFor='pifaqs'>Pi FAQs</label>
-      </NavLink>
-      <NavLink to='/map' activeStyle={{
-        fontWeight: 'bold',
-        color: 'red'
-      }}>
-        <input id='map' type="radio" name='nav' value='map'  onChange={handleChange} />
-        <label htmlFor='map'>Data Map</label>
-      </NavLink>
-      <NavLink to='/montecarlo' activeStyle={{
-        fontWeight: 'bold',
-        color: 'red'
-      }}>
-        <input id='monteCarlo' type="radio" name='nav' value='monteCarlo'  onChange={handleChange} />
-        <label htmlFor='monteCarlo'>Monte Carlo</label>
-      </NavLink>
-    </>
-  );
+  const [publicFacingNav, setPublicFacingNav] = useState(false);
+  let jsx;
+  if(!publicFacingNav){
+    jsx = (
+      <div className={styles.Big}>
+        <span onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
+        <span onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
+        <span onClick={() => setPublicFacingNav(!publicFacingNav)}>|</span>
+      </div>);
+  }
+  if(publicFacingNav){
+    jsx = (
+      <div className={styles.NavBackground}>
+        <div id='jsxHorizontal' className={styles.Collumn}>
+          <span style={{ color: 'white', textDecoration: 'none' }} onClick={() => setPublicFacingNav(!publicFacingNav)}>|||</span>
+        </div>
+        <ul className={styles.Nav}>
+          <NavLink style={{ color: 'white', textDecoration: 'none' }} exact to='/' activeStyle={{
+            fontWeight: 'bold',
+            color: 'purple'
+          }}>
+            <input style={{ display: 'none' }} id='home' value='home' onChange={handleChange} />
+            <label htmlFor='home'>Home</label>
+          </NavLink>
+          <NavLink style={{ color: 'white', textDecoration: 'none' }}  to='/teachers' activeStyle={{
+            fontWeight: 'bold',
+            color: 'purple'
+          }}>
+            <input style={{ display: 'none' }} id='teachers' value='teachers'  onChange={handleChange} />
+            <label htmlFor='teachers'>For Teachers</label>
+          </NavLink>
+          <NavLink style={{ color: 'white', textDecoration: 'none' }}  to='/pifaqs' activeStyle={{
+            fontWeight: 'bold',
+            color: 'purple'
+          }}>
+            <input style={{ display: 'none' }} id='pifaqs' value='pifaqs'  onChange={handleChange} />
+            <label htmlFor='pifaqs'>Pi FAQs</label>
+          </NavLink>
+          <NavLink style={{ color: 'white', textDecoration: 'none' }}  to='/map' activeStyle={{
+            fontWeight: 'bold',
+            color: 'purple'
+          }}>
+            <input style={{ display: 'none' }} id='map' value='map'  onChange={handleChange} />
+            <label htmlFor='map'>Data Map</label>
+          </NavLink>
+          <NavLink style={{ color: 'white', textDecoration: 'none' }}  to='/montecarlo' activeStyle={{
+            fontWeight: 'bold',
+            color: 'purple'
+          }}>
+            <input style={{ display: 'none' }} id='monteCarlo' value='monteCarlo'  onChange={handleChange} />
+            <label htmlFor='monteCarlo'>Monte Carlo</label>
+          </NavLink>
+        </ul>
+      </div>);
+  }
+  return (jsx);
 };
 
 export default Nav;
