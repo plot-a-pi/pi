c5856b7f6d210ba8e1826934fbb76ca9c522603a
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-02-24 19:10:09 -0800

    revert app to hello world

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import DataEntryForm from '../components/controls/DataEntryForm';
-import Stats from '../components/home/Stats';
-import CircumferenceVsDiameterGraph from '../components/graphs/CircumferenceVsDiameterGraph';
-import GlobalPiVsCountGraph from './graphs/GlobalPiVsCountGraph';
 
 export default function App() {
   return (
-    <>
-      <DataEntryForm />
-      <Stats />
-      <CircumferenceVsDiameterGraph />
-      <GlobalPiVsCountGraph />
-    </>
+    <h1>Hello World</h1>
     
   );
 }
