648cbffd9224aa3384e6740ab9a575ebb3fdd84c
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-02-26 15:51:53 -0800

    remove header import from Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '../common/Header';
 import Nav from '../common/Nav';
 import ButtonNav from './ButtonNav';
 import CircumferenceVsDiameterWrapper from '../graphs/CircumferenceVsDiameterWrapper';
