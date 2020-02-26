fb5510a343172aa5b2f94011f4f4de680175a7cd
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-02-26 07:53:14 -0800

    revert app

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,17 @@ import Home from './home/Home';
 import SessionForm from './controls/SessionForm';
 import DataEntryForm from './controls/DataEntryForm';
 import SessionGraph from './graphs/SessionGraph';
-import GlobalPiVsCountGraph from './graphs/GlobalPiVsCountGraph';
-import CircumferenceVsDiameterWrapper from './graphs/CircumferenceVsDiameterWrapper';
 
 export default function App() {
   return (
     <>
-      <CircumferenceVsDiameterWrapper />
-      {/* <Switch>
+      <Switch>
         <Route exact path='/' component={Home} />
         
         <Route path='/session/:id' component={SessionForm} />
         <Route path='/submit-to-global' component={DataEntryForm} />
         <Route path='/session-graph/:id' component={SessionGraph} />
-      </Switch> */}
+      </Switch>
     </>
   );
 }
