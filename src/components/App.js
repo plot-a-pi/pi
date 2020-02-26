b9b80c78518ee8d096dca3b05752ff59bf3dd1ac
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-02-26 15:50:03 -0800

    changes to header styling and SessionForm connected to form css

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './home/Home';
 import SessionForm from './controls/SessionForm';
 import DataEntryForm from './controls/DataEntryForm';
-// import Header from './common/Header';
+import Header from './common/Header';
 import SessionGraph from './graphs/SessionGraph';
 import TeacherSessions from './teacherDashboard/TeacherSessions';
 import TeacherDashboard from './teacherDashboard/TeacherDashboard';
@@ -11,18 +11,21 @@ import GraphLabelWrapper from './common/GraphLabelWrapper';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='/teachers' component={TeacherDashboard} />
-      <Route path='/teacher-sessions' component={TeacherSessions} />
-      <Route path='/session/:id' component={SessionForm} />
-      <Route path='/submit-to-global' component={DataEntryForm} />
-      <Route path='/session-graph/:id' render = {() => (
-        <GraphLabelWrapper title='Global' xLabel='x' yLabel='y'>
-          <SessionGraph />
-        </GraphLabelWrapper>
-      )} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/teachers' component={TeacherDashboard} />
+        <Route path='/teacher-sessions' component={TeacherSessions} />
+        <Route path='/session/:id' component={SessionForm} />
+        <Route path='/submit-to-global' component={DataEntryForm} />
+        <Route path='/session-graph/:id' render = {() => (
+          <GraphLabelWrapper title='Global' xLabel='x' yLabel='y'>
+            <SessionGraph />
+          </GraphLabelWrapper>
+        )} />
+      </Switch>
+    </>
   );
 
 }
