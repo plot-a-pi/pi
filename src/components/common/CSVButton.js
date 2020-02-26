fb7ac884d31a0ef069640cb67667ea01293ccad2
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-26 10:45:05 -0800

    csv button

--- src/components/common/CSVButton.js
+++ src/components/common/CSVButton.js
@@ -1,18 +1,16 @@
-// import React from 'react';
-// import CSV from './CSV';
-// import { globalDataCollection } from '../../firebase/firebase';
+import React from 'react';
+import CSV from './CSV';
 
-// const CSVButton = () => {
+const CSVButton = ({ children }) => {
 
-//   return (
-//     <>
-//       <button> Download Data
-//         <CSV ref={globalDataCollection } />
-//       </button>
+  return (
+    <>
+      <button> Download Icon
+        {children}
+      </button>
+    </>
+  );
 
-//     </>
-//   );
+};
 
-// };
-
-// export default CSVButton;
+export default CSVButton;
