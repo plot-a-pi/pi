f836a504123fcfd780f33d019e6480bf8bb83c19
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-02-26 14:33:21 -0800

    restore App.js to its proper placve

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,36 @@
 import React from 'react';
-// import { Switch, Route } from 'react-router-dom';
-// import Home from './home/Home';
-// import SessionForm from './controls/SessionForm';
+import { Switch, Route } from 'react-router-dom';
+import Home from './home/Home';
+import SessionForm from './controls/SessionForm';
 import DataEntryForm from './controls/DataEntryForm';
-import Header from './common/Header';
-// import SessionGraph from './graphs/SessionGraph';
-// import { GraphLabelWrapper } from './common/GraphLabelWrapper';
+// import Header from './common/Header';
+import SessionGraph from './graphs/SessionGraph';
+import { GraphLabelWrapper } from './common/GraphLabelWrapper';
 
 export default function App() {
   return (
     <>
-      <Header />
-      <DataEntryForm />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/session/:id' component={SessionForm} />
+        <Route path='/submit-to-global' component={DataEntryForm} />
+        <Route path='/session-graph/:id' render = {() => (
+          <GraphLabelWrapper title='Global' xLabel='x' yLabel='y'>
+            <SessionGraph />
+          </GraphLabelWrapper>
+        )} />
+      </Switch>
     </>
   );
+
 }
 
 // return (
 //   <>
-//     <Switch>
-//       <Route exact path='/' component={Home} />
-//       <Route path='/session/:id' component={SessionForm} />
-//       <Route path='/submit-to-global' component={DataEntryForm} />
-//       <Route path='/session-graph/:id' render = {() => (
-//         <GraphLabelWrapper title='Global' xLabel='x' yLabel='y'>
-//           <SessionGraph />
-//         </GraphLabelWrapper>
-//       )} />
-//     </Switch>
+//     <Header />
+//     <DataEntryForm />
 //   </>
 // );
+
+
 
