185e91a02c8df74daf7c96f24a7353ef10881165
Jacob Harrington <51795865+yaycub@users.noreply.github.com>
2020-02-27 11:51:48 -0800

    Features/socket io (#63)
    * add file structure for components
    
    * mutable getData service complete
    
    * changed service name to getData
    
    * removed service from dev
    
    * mean calculation
    
    * tests passing
    
    * started working on service and component with Lisa
    
    * circumference vs diameter graph rendering
    
    * circumference vs diameter graph finished
    
    * test passing
    
    * changed app.js back
    
    * dual arrays feeding graph
    
    * graph resizing working
    
    * update snapshots
    
    * decouple circumferencevsdiameter from firebase
    
    * front end now working with global stats via socket
    
    * removed unused import statements due to socket/mongoDB migration
    
    * reducer tests complete
    
    * all tests passing before socket implementation
    
    * mock seed working, ready for socket implementation
    
    * updated with session form working
    
    * fix line issue
    
    * firebase has been removed
    
    * updated some tests. commiting to pull for merge conflicts
    
    * all tests passing branch ready for merge
    
    Co-authored-by: Lisa Carpenter <48000707+licarpen@users.noreply.github.com>
    Co-authored-by: Tali Avni <54651549+avnit77@users.noreply.github.com>
    Co-authored-by: Jasmin Moneymaker <54380347+JMoneymaker@users.noreply.github.com>

--- src/components/controls/DataEntryForm.test.js
+++ src/components/controls/DataEntryForm.test.js
@@ -3,6 +3,12 @@ import { mount } from 'enzyme';
 import DataEntryForm from './DataEntryForm';
 import { BrowserRouter } from 'react-router-dom';
 
+jest.mock('react-socket-io-hooks', () => ({
+  useEmitEvent: () => {}, 
+  useSocketState: () => {}, 
+  useSocket: () => {}
+}));
+
 describe('DataEntryForm component', () => {
   it('renders the DataEntryForm', () => {
     const wrapper = mount(
