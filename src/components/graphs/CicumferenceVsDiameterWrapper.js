09da239e3497d3c7860b31562a9f041e12a28288
Jasmin Moneymaker <54380347+JMoneymaker@users.noreply.github.com>
2020-02-27 14:38:09 -0800

    graph styling in progress (#75)

--- src/components/graphs/CicumferenceVsDiameterWrapper.js
+++ src/components/graphs/CicumferenceVsDiameterWrapper.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import CircumferenceVsDiameterGraph from '../graphs/CircumferenceVsDiameterGraph';
 import { useEmitEvent, useSocketState, useSocket } from 'react-socket-io-hooks';
+import graphContainerStyles from './CircumferenceVsDiameter.css';
+import CvDGraphStats from './CvDGraphStats';
+
 
 const CircumferenceVsDiameterWrapper = () => {
   const emitRetrievedDataPoints = useEmitEvent('RETRIEVE_DATA_POINTS');
@@ -17,9 +20,29 @@ const CircumferenceVsDiameterWrapper = () => {
   }, [socket.connected]);
 
   return (
-    <div>
-      <CircumferenceVsDiameterGraph data={points} stats={stats} xLabel='Diameter (cm)' yLabel='Circumference (cm)' title='Global Data for Circumference Vs. Diameter' />
-    </div>
+    <>
+      <CvDGraphStats stats={stats}/>
+      <div className={graphContainerStyles.GraphGridMock}>
+        <div className={graphContainerStyles.gridContainer}>
+          <div className={graphContainerStyles.yLabel}>
+            <p>Circumference (cm)</p> 
+          </div>
+          <div className={graphContainerStyles.title}>
+            <h2>Global Data for Circumference Vs Diameter</h2>
+          </div>
+          <div className={graphContainerStyles.graph}>
+            <section>
+              <div>
+                <CircumferenceVsDiameterGraph data={points} stats={stats} />
+              </div>
+            </section>
+          </div>
+          <div className={graphContainerStyles.xLabel}>
+            <p>Diameter (cm)</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
