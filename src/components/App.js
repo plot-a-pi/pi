59d348a733a415a7f1ce38b4ed97c20eab0fcb95
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-02-27 10:26:11 -0800

    switching to dev

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,31 @@
 import React from 'react';
-// import { Switch, Route } from 'react-router-dom';
-// import Home from './home/Home';
-// import SessionForm from './controls/SessionForm';
-// import DataEntryForm from './controls/DataEntryForm';
-// import Header from './common/Header';
-// import SessionGraph from './graphs/SessionGraph';
-// import TeacherSessions from './teacherDashboard/TeacherSessions';
-// import TeacherDashboard from './teacherDashboard/TeacherDashboard';
-// import { withSession } from '../firebase/AuthProvider';
-import GraphGridMock from './graphs/GraphGridMock';
+import { Switch, Route } from 'react-router-dom';
+import Home from './home/Home';
+import SessionForm from './controls/SessionForm';
+import DataEntryForm from './controls/DataEntryForm';
+import Header from './common/Header';
+import SessionGraph from './graphs/SessionGraph';
+import TeacherSessions from './teacherDashboard/TeacherSessions';
+import TeacherDashboard from './teacherDashboard/TeacherDashboard';
+import { withSession } from '../firebase/AuthProvider';
+// import GraphGridMock from './graphs/GraphGridMock';
 
 export default function App() {
   return (
     <>
-      <GraphGridMock />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/teachers' component={TeacherDashboard} />
+        <Route path='/teacher-sessions' component={withSession(TeacherSessions)} />
+        <Route path='/session/:id' component={SessionForm} />
+        <Route path='/submit-to-global' component={DataEntryForm} />
+        <Route path='/session-graph/:id' render = {() => (
+          <SessionGraph />
+        )} />
+      </Switch>
     </>
   );
 }
 
-// return (
-//   <>
-//     <Header />
-//     <Switch>
-//       <Route exact path='/' component={Home} />
-//       <Route path='/teachers' component={TeacherDashboard} />
-//       <Route path='/teacher-sessions' component={withSession(TeacherSessions)} />
-//       <Route path='/session/:id' component={SessionForm} />
-//       <Route path='/submit-to-global' component={DataEntryForm} />
-//       <Route path='/session-graph/:id' render = {() => (
-//         <SessionGraph />
-//       )} />
-//     </Switch>
-//   </>
-// );
+// 
