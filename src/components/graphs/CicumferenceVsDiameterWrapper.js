ae694ab8080c3aefbb1b8ebc8acafe33274dc612
Tali Avni <tali@aristo7.com>
2020-02-27 11:22:13 -0800

    graph display on home

--- src/components/graphs/CicumferenceVsDiameterWrapper.js
+++ src/components/graphs/CicumferenceVsDiameterWrapper.js
@@ -1,14 +1,15 @@
-  
+
 import React from 'react';
 import { useFirestore } from '../../firebase/hooks';
 import { globalDataCollection, globalStatsCollection } from '../../firebase/firebase';
 import CircumferenceVsDiameterGraph from '../graphs/CircumferenceVsDiameterGraph';
+import styles from '../home/Home.css'
 
 const CircumferenceVsDiameterWrapper = () => {
   const data = useFirestore(globalDataCollection, []);
   const stats = useFirestore(globalStatsCollection.doc('current-stats'), { circumferenceMax: 50, diameterMax: 50 });
   return (
-    <div>
+    <div className={styles.circ}>
       <CircumferenceVsDiameterGraph data={data} stats={stats} xLabel='Diameter (cm)' yLabel='Circumference (cm)' title='Global Data for Circumference Vs. Diameter' />
     </div>
   );
