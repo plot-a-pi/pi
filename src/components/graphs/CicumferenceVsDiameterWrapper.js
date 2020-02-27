a9bd12e64570278675e2cd97247296c78d1b11f6
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-27 09:37:49 -0800

    CSVButton on D/C graph

--- src/components/graphs/CicumferenceVsDiameterWrapper.js
+++ src/components/graphs/CicumferenceVsDiameterWrapper.js
@@ -3,14 +3,20 @@ import React from 'react';
 import { useFirestore } from '../../firebase/hooks';
 import { globalDataCollection, globalStatsCollection } from '../../firebase/firebase';
 import CircumferenceVsDiameterGraph from '../graphs/CircumferenceVsDiameterGraph';
+import CSVButton from '../common/CSVButton';
 
 const CircumferenceVsDiameterWrapper = () => {
   const data = useFirestore(globalDataCollection, []);
   const stats = useFirestore(globalStatsCollection.doc('current-stats'), { circumferenceMax: 50, diameterMax: 50 });
+  const dataForCSV = data.map(datum => ([datum.diameter, datum.circumference]));
+  console.log(dataForCSV);
   return (
-    <div>
-      <CircumferenceVsDiameterGraph data={data} stats={stats} xLabel='Diameter (cm)' yLabel='Circumference (cm)' title='Global Data for Circumference Vs. Diameter' />
-    </div>
+    <>
+      <CSVButton header1='Diameter' header2='Circumference' data={dataForCSV} />
+      <div>
+        <CircumferenceVsDiameterGraph data={data} stats={stats} xLabel='Diameter (cm)' yLabel='Circumference (cm)' title='Global Data for Circumference Vs. Diameter' />
+      </div>
+    </>
   );
 };
 
