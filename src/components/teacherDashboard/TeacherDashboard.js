398adc817b9dd1081798bfb64eaa08ab805ee188
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-02-26 19:27:29 -0800

    teacher dash styling in place

--- src/components/teacherDashboard/TeacherDashboard.js
+++ src/components/teacherDashboard/TeacherDashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useUser } from '../../firebase/AuthProvider';
 import { useHistory } from 'react-router-dom';
 import { loginWithProvider } from '../../firebase/firebase';
+import styles from './TeacherDashboard.css';
 
 const TeacherDashboard = () => {
   const user = useUser();
@@ -17,8 +18,15 @@ const TeacherDashboard = () => {
 
   return (
     <>
-      <button onClick={handleClick}>SignUp/Login</button>
-      <p>Some Intructions</p>
+      <div className={styles.TeacherDashboard}>
+        <div className={styles.instructions}>
+          <h2>Instructions</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div className={styles.loginButton}>
+          <button onClick={handleClick}>SignUp/Login</button>
+        </div>
+      </div>
     </>
   );
 };
