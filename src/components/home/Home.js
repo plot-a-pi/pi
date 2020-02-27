c0a3e0c8567f6ffca11722452da3ce01944a5e61
alanhermanns <55249127+alanhermanns@users.noreply.github.com>
2020-02-26 16:08:14 -0800

    Svg/labels (#50)
    * hamburger
    
    * purple
    
    * test
    
    * pulled
    
    * csv button
    
    * csv button w proptypes and data
    
    * merge
    
    * proptypes on graphlabel wrapper
    
    * button on click
    
    * svg height
    
    * onte carlo lables
    
    * resize
    
    * cvsd graph labesl
    
    * test updated :id goes to monte carlo
    
    * test updated :id goes to monte carlo
    
    * test updated :id goes to monte carlo
    
    * test update
    
    * removed x-y max from montecarlo
    
    * app back to std
    
    Co-authored-by: ah56 <53789900+ah56@users.noreply.github.com>

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,9 +11,7 @@ const Home = () => {
     <>
       <Nav />
       <Header />
-      <GraphLabelWrapper title='globl' xLabel='Diameter' yLabel='Circumfrence'>
-        <CircumferenceVsDiameterWrapper />
-      </GraphLabelWrapper>
+      <CircumferenceVsDiameterWrapper />
       <ButtonNav />
     </>
   );
