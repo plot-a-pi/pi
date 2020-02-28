ae1fe668ddec538c2b27737248a18d5792880c8e
Jacob Harrington <51795865+yaycub@users.noreply.github.com>
2020-02-27 16:14:19 -0800

    Feature/user point socket (#76)
    * mutable getData service complete
    
    * changed service name to getData
    
    * removed service from dev
    
    * user uploaded point added to socket reducer
    
    * updated tests for travis
    
    * fixing error
    
    * conflicts resolved

--- src/components/controls/DataEntryForm.js
+++ src/components/controls/DataEntryForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './DataEntryForm.css';
 import { useFormInput } from '../../hooks/useFormInput';
-import { globalStatsCollection } from '../../firebase/firebase';
 import Modal from '../common/Modal';
 import { useModal } from '../../hooks/useModal';
 import { useHistory } from 'react-router-dom';
@@ -27,25 +26,13 @@ const DataEntryForm = () => {
     if(circumferenceAsNumber < diameterAsNumber) return alert('Are you sure your measurements are correct?');
     if(Math.abs(circumferenceAsNumber / diameterAsNumber) > 0.15) return alert('Are you sure you measured accurately?');
 
-
-    globalStatsCollection.doc('current-stats').get().then((stats) => {
-      if(localStorage.getItem('my-point-ids')){
-        const pointIds = JSON.parse(localStorage.getItem('my-point-ids'));
-        const updatedPointIds = pointIds.concat([stats.data().count + 1]);
-        localStorage.setItem('my-point-ids', JSON.stringify(updatedPointIds));
-      } else {
-        localStorage.setItem('my-point-ids', JSON.stringify([stats.data().count + 1]));
+    emitCreateDataPoint({
+      payload: {
+        circumference: Number(circumference),
+        diameter: Number(diameter),
+        circumferenceUnit,
+        diameterUnit
       }
-
-      emitCreateDataPoint({
-        payload: {
-          pointId: stats.data().count + 1,
-          circumference: Number(circumference),
-          diameter: Number(diameter),
-          circumferenceUnit,
-          diameterUnit
-        }
-      });
     });
 
     resetCircumference();
--- src/reducer.js
+++ src/reducer.js
@@ -10,7 +10,8 @@ export const initialState = {
   sessionStats: {
     circumferenceMax: null,
     diameterMax: null
-  }
+  },
+  recentUserPoint: null
 };
 
 export const UPDATE_GLOBAL = 'UPDATE_GLOBAL';
@@ -26,7 +27,7 @@ export const GET_SESSION_STATS = 'GET_SESSION_STATS';
 export default function reducer(state, action) {
   switch(action.type){
     case UPDATE_GLOBAL:
-      return { ...state, points: [...state.points, action.payload] };
+      return { ...state, points: [...state.points, action.payload],  recentUserPoint: action.payload };
     case RETRIEVE_DATA_POINTS:
       return { ...state, points: action.payload };
     case RETRIEVE_GLOBAL_STATS:
