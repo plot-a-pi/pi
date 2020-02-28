396f5dca94cd48f5baa82a947d61851a67f90556
alanhermanns <55249127+alanhermanns@users.noreply.github.com>
2020-02-27 18:16:55 -0800

    Session graph (#87)
    * hamburger
    
    * purple
    
    * test
    
    * pulled
    
    * remove graph wrapper
    
    * labels sessiongraph
    
    * labels
    
    * no title
    
    Co-authored-by: ah56 <53789900+ah56@users.noreply.github.com>

--- src/components/graphs/SessionGraph.js
+++ src/components/graphs/SessionGraph.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Scatterplot from './Scatterplot';
 import PropTypes from 'prop-types';
 import { useEmitEvent, useSocket, useSocketState } from 'react-socket-io-hooks';
-import styles from '../home/Home.css';
+import styles from '../graphs/CircumferenceVsDiameter.css';
 
 const SessionGraph = ({ match }) => {
   const { id } = match.params;
@@ -37,16 +37,29 @@ const SessionGraph = ({ match }) => {
   let dataArray = [];
 
   dataArray = (sessionData.map(point => [point.circumference, point.diameter]));
-
   return (
     <>
-      <div className={styles.graphs}>
-        <Scatterplot data={dataArray} xMax={xMax} yMax ={yMax}/>
+      <div className={styles.GraphGridMock}>
+        <div className={styles.gridContainer}>
+          <div className={styles.yLabel}>
+            <p>Circumference</p> 
+          </div>
+          <div className={styles.graph}>
+            <section>
+              <div className={styles.graphs}>
+                <Scatterplot data={dataArray} xMax={xMax} yMax ={yMax}/>
+              </div>
+            </section>
+          </div>
+          <div className={styles.xLabel}>
+            <p>Diameter</p>
+          </div>
+        </div>
       </div>
     </>
   );
-};
 
+};
 SessionGraph.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
