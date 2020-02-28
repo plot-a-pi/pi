4044d6108a2dd342b868982793dd70307f533de3
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-02-27 17:43:06 -0800

    labels sessiongraph

--- src/components/graphs/SessionGraph.js
+++ src/components/graphs/SessionGraph.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Scatterplot from './Scatterplot';
 import PropTypes from 'prop-types';
 import { useEmitEvent, useSocket, useSocketState } from 'react-socket-io-hooks';
-import styles from '../home/Home.css';
+import styles from '../graphs/CircumferenceVsDiameter.css';
 
 const SessionGraph = ({ match }) => {
   const { id } = match.params;
@@ -27,16 +27,32 @@ const SessionGraph = ({ match }) => {
   let dataArray = [];
 
   dataArray = (sessionData.map(point => [point.circumference, point.diameter]));
-
   return (
     <>
-      <div className={styles.graphs}>
-        <Scatterplot data={dataArray} xMax={xMax} yMax ={yMax}/>
+      <div className={styles.GraphGridMock}>
+        <div className={styles.gridContainer}>
+          <div className={styles.yLabel}>
+            <p>Y-Label</p> 
+          </div>
+          <div className={styles.title}>
+            <h2>Title</h2>
+          </div>
+          <div className={styles.graph}>
+            <section>
+              <div className={styles.graphs}>
+                <Scatterplot data={dataArray} xMax={xMax} yMax ={yMax}/>
+              </div>
+            </section>
+          </div>
+          <div className={styles.xLabel}>
+            <p>X-Label</p>
+          </div>
+        </div>
       </div>
     </>
   );
-};
 
+};
 SessionGraph.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
