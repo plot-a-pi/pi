a06a0491d1e050503e5b1cf0b9881bb24e3c6d09
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-16 16:34:58 -0700

    style header and nav

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import Modal from '../common/Modal';
-import { useModal } from '../../hooks/useModal';
 import styles from './Home.css';
 import PiCrawler from '../common/PiCrawler';
 import { useEmitEvent, useSocketState, useSocket } from 'react-socket-io-hooks';
@@ -15,16 +13,6 @@ const Home = () => {
   const socket = useSocket();
   const { points } = useSocketState();
   const { stats } = useSocketState();
-  const [showIntroModal, toggleIntroModal] = useModal();
-
-  const modalInstructions = (
-    <div className={styles.modal}>
-      <h3>Would you like to contribute to pi?</h3>
-      <br/>
-      <p>Measure the circumference and diamter of a circular object and submit your measurements to improve our global approximation of pi.</p>
-      <br/>
-      <p>How does the approximation change as more data is added?</p>
-    </div>);
 
   useEffect(() => {
     if(socket.connected !== undefined) {
@@ -39,13 +27,6 @@ const Home = () => {
   
   return (
     <div className={styles.Home}>
-      <section className={styles.introduction}>
-        <div className={styles.whatsThis}>
-          <h3>What&apos;s this about...</h3>
-          <button className={styles.modalButton} type='button' onClick={() => toggleIntroModal()}> ? </button>
-        </div>
-        <Modal showModal={showIntroModal} toggleModal={toggleIntroModal} modalTitle={'Diameter'} modalInstructions={modalInstructions} />
-      </section>
       <CvsDScatterplot className={styles.graphA} data={points} stats={stats} title={'Global Circle Measurements'} xLabel='Diameter (in)' yLabel={'Circumference (in)'} /> 
       <CvDGraphStats className={stats} stats={stats}/>
       <PivsCountScatterplot className={styles.graphB} data={dataArray} title={'Global Pi Approximation vs Count'} xLabel={'Count'} yLabel={'Pi Approximation'} />
