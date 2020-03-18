7414cdd05d6a0c7d2ce5256e4921b9e2a1545274
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-18 11:32:47 -0700

    add conversions function convert to support unit conversion

---
+++ src/data/conversions.js
@@ -0,0 +1,9 @@
+export const convert = (point) => {
+  // const conversionObject = {
+  //   cmToIn : x => x / 2.54,
+  //   inToCm: x => x * 2.54 
+  // };
+  if(point.circumferenceUnit === 'in')
+    return { ...point, circumference: point.circumference * 2.54, diameter: point.diameter * 2.54 };
+  else return point;
+};
